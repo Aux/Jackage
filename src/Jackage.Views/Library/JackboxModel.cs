43d40f1b14b3ba040e2ddfadc4541d1ae4104bf7
Aux <justin@akitaux.com>
2023-06-10 01:22:28 -0500

    Remove unused project

--- src/Jackage.Views/Library/JackboxModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Jackage.Views;
-
-public class JackboxModel
-{
-    public string Name { get; set; }
-    public string CoverArt { get; set; }
-    public bool IsAvailable { get; set; }
-    public bool IsImplemented { get; set; }
-}
